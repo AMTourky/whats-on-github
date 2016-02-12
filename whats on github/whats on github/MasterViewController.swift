4f2740c4d3c3b408fc854cd80d5f3ff99986ce92
AMTourky <abdelhameed.tourky@radius60.com>
2016-02-13 01:57:37 +0200

    show search results

--- whats on github/whats on github/MasterViewController.swift
+++ whats on github/whats on github/MasterViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class MasterViewController: UITableViewController, UISearchBarDelegate {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+    @IBOutlet var searchBar: UISearchBar?
     var repositoriesSerivce: GithubRepositoriesService = GithubRepositoriesService()
+    var repositories: [Repository] = [Repository]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,20 +35,15 @@ class MasterViewController: UITableViewController, UISearchBarDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
 
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let repo = self.repositories[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = repo
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -61,24 +57,34 @@ class MasterViewController: UITableViewController, UISearchBarDelegate {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return self.repositories.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let repo = self.repositories[indexPath.row]
+        cell.textLabel!.text = repo.name
         return cell
     }
     
     // MARK: - Search Bar Delegate
-    func searchBar(searchBar: UISearchBar, textDidChange searchText: String)
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar)
     {
-        repositoriesSerivce.getRepositoryForLanguage(searchText) { (error, repositories) -> Void in
-            print(repositories)
+        if let theSearchBar = self.searchBar, theSearchTerm = theSearchBar.text
+        {
+            repositoriesSerivce.getRepositoryForLanguage(theSearchTerm) { (error, repositories) -> Void in
+                print(repositories)
+                guard let theRepositories = repositories where theRepositories.count > 0
+                else
+                {
+                    return
+                }
+                self.repositories = theRepositories
+                self.tableView.reloadData()
+            }
         }
     }
-
 }
 
